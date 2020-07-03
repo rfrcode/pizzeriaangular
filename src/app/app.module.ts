c06506785b3e0fd3123106633e5c4626358c3546
rfrcode <60968161+rfrcode@users.noreply.github.com>
2020-07-03 14:06:58 +0200

    add password validator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module'
 
-const LOGINVALIDATOR = {
+const LOGINVALIDATOR = [{
   fields: ["email"],
   message: "email es requerido",
   validators: [{ sanitizer: false, validator: () => true, args: [1, 50] }]
-}
+},
+{
+  fields: ["password"],
+  message: "password es requerido",
+  validators: [{ sanitizer: false, validator: () => true, args: [1, 10] }]
+}]
 
 @NgModule({
   declarations: [
